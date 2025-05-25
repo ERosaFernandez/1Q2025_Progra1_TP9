8e1364998ebeeff9f79398b493ed35098752474f
Enzorofer <erosafernandez@itba.edu.ar>
2025-05-25 15:53:28 -0300

    Ejercicio 5 V1

--- Ejercicio5/Main.c
+++ Ejercicio5/Main.c
@@ -8,6 +8,24 @@
 #include "Puertos.h"
 
 int main (){
-	printall();
+	maskOn (D, 27167);
+	printf ("%d\n", (uint16_t) getValue (D));
+	printf ("%d\n", (uint16_t) getValue (A));
+	printf ("%d\n", (uint16_t) getValue (B));
+	printall (D);
+	printall (A);
+	printall (B);
+	bitSet(A,2);
+	printf ("%d\n", (uint16_t) getValue (A));
+	bitSet('C',3); //error a proposito
+	bitToggle(A,1);
+	bitSet(B,6);
+	bitClr(B,5);
+	printall (D);
+	maskOff (D, 2153);
+	printall (D);
+	maskToggle(D, 33915);
+	printall (D);
+
 	return 0;
 }
